36b4e8ef04e16d2d810e45c90077033b0b28565b
Ryan <ryan6tk@hotmail.co.uk>
2017-07-21 00:04:43 +0100

    Ported code.

---
+++ posts.ts
@@ -0,0 +1,19 @@
+class Posts {
+	public static readonly ALL = [
+		{ filename: "04-06-2016", tags: ["underrated", "managers", "part", "3", "leonardo", "jardim"] },
+		{ filename: "04-11-2016", tags: ["shkodran", "mustafi", "good", "bad", "ugly"] }
+	];
+
+	public static readonly WRITEUPS = [
+		{ filename: "04-06-2016", tags: ["underrated", "managers", "part", "3", "leonardo", "jardim"] },
+		{ filename: "04-11-2016", tags: ["shkodran", "mustafi", "good", "bad", "ugly"] }
+	];
+
+	public static readonly PLAYERS = [
+		{ filename: "", tags: [""] }
+	];
+
+	public static readonly BETTING = [
+		{ filename: "", tags: [""] }
+	];
+}
