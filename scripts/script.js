2540a40fd694e61712b6f6b8a0327609255efcc8
Ryan <ryan6tk@hotmail.co.uk>
2017-08-11 11:10:07 +0100

    Port.

--- scripts/script.js
+++ scripts/script.js
@@ -11,15 +11,17 @@ var Posts = (function () {
         { filename: "16-01-2015", tags: ["underrated", "managers", "part", "2", "francesco", "guidolin"] },
         { filename: "04-06-2016", tags: ["underrated", "managers", "part", "3", "leonardo", "jardim"] },
         { filename: "04-11-2016", tags: ["shkodran", "mustafi", "good", "bad", "ugly"] },
+        { filename: "11-08-2017", tags: ["site", "introduction", "direction", "intacttactic"] },
+        { filename: "11-08-2017-2", tags: ["arsenal", "season", "preview", "2017/18", "3-4-2-1"] }
     ];
     Posts.WRITEUPS = [
         { filename: "03-01-2015", tags: ["underrated", "managers", "part", "1", "unai", "emery"] },
         { filename: "16-01-2015", tags: ["underrated", "managers", "part", "2", "francesco", "guidolin"] },
         { filename: "04-06-2016", tags: ["underrated", "managers", "part", "3", "leonardo", "jardim"] },
-        { filename: "04-11-2016", tags: ["shkodran", "mustafi", "good", "bad", "ugly"] },
+        { filename: "11-08-2017-2", tags: ["arsenal", "season", "preview", "2017/18", "3-4-2-1"] }
     ];
     Posts.PLAYERS = [
-        { filename: "", tags: [""] }
+        { filename: "04-11-2016", tags: ["shkodran", "mustafi", "good", "bad", "ugly"] }
     ];
     Posts.BETTING = [
         { filename: "", tags: [""] }
