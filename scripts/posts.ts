146c0561366793d80057bd5d49ce144512bbae34
Ryan <ryan6tk@hotmail.co.uk>
2017-08-13 14:21:33 +0100

    Port.

--- scripts/posts.ts
+++ scripts/posts.ts
@@ -12,7 +12,8 @@ class Posts {
 		{ filename: "11-08-2017", tags: ["site", "introduction", "direction", "intacttactic"] },
 		{ filename: "11-08-2017-2", tags: ["arsenal", "season", "preview", "2017/18", "3-4-2-1"] },
 		{ filename: "12-08-2017", tags: ["serie", "napoli", "time", "title", "juventus", "ac milan", "roma"] },
-		{ filename: "12-08-2017-2", tags: ["bernardo", "silva"] },
+		{ filename: "12-08-2017-2", tags: ["bernardo", "silva", "summary"] },
+		{ filename: "13-08-2017", tags: ["west", "ham", "short", "term", "improvement"] },
 	];
 
 	public static readonly WRITEUPS = [
@@ -21,11 +22,12 @@ class Posts {
 		{ filename: "04-06-2016", tags: ["underrated", "managers", "part", "3", "leonardo", "jardim"] },
 		{ filename: "11-08-2017-2", tags: ["arsenal", "season", "preview", "2017/18", "3-4-2-1"] },
 		{ filename: "12-08-2017", tags: ["serie", "napoli", "time", "title", "juventus", "ac milan", "roma"] },
+		{ filename: "13-08-2017", tags: ["west", "ham", "short", "term", "improvement"] },
 	];
 
 	public static readonly PLAYERS = [
 		{ filename: "04-11-2016", tags: ["shkodran", "mustafi", "good", "bad", "ugly"] },
-		{ filename: "12-08-2017-2", tags: ["bernardo", "silva"] },
+		{ filename: "12-08-2017-2", tags: ["bernardo", "silva", "summary"] },
 	];
 
 	public static readonly BETTING = [
--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,8 @@ var Posts = (function () {
         { filename: "11-08-2017", tags: ["site", "introduction", "direction", "intacttactic"] },
         { filename: "11-08-2017-2", tags: ["arsenal", "season", "preview", "2017/18", "3-4-2-1"] },
         { filename: "12-08-2017", tags: ["serie", "napoli", "time", "title", "juventus", "ac milan", "roma"] },
-        { filename: "12-08-2017-2", tags: ["bernardo", "silva"] },
+        { filename: "12-08-2017-2", tags: ["bernardo", "silva", "summary"] },
+        { filename: "13-08-2017", tags: ["west", "ham", "short", "term", "improvement"] },
     ];
     Posts.WRITEUPS = [
         { filename: "03-01-2015", tags: ["underrated", "managers", "part", "1", "unai", "emery"] },
@@ -22,10 +23,11 @@ var Posts = (function () {
         { filename: "04-06-2016", tags: ["underrated", "managers", "part", "3", "leonardo", "jardim"] },
         { filename: "11-08-2017-2", tags: ["arsenal", "season", "preview", "2017/18", "3-4-2-1"] },
         { filename: "12-08-2017", tags: ["serie", "napoli", "time", "title", "juventus", "ac milan", "roma"] },
+        { filename: "13-08-2017", tags: ["west", "ham", "short", "term", "improvement"] },
     ];
     Posts.PLAYERS = [
         { filename: "04-11-2016", tags: ["shkodran", "mustafi", "good", "bad", "ugly"] },
-        { filename: "12-08-2017-2", tags: ["bernardo", "silva"] },
+        { filename: "12-08-2017-2", tags: ["bernardo", "silva", "summary"] },
     ];
     Posts.BETTING = [
         { filename: "", tags: [""] }
